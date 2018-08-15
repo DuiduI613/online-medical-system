160ce9d8ee76e7d022f6e836e81ea2337f667bad
Zhitian Luo <zhitian.luo.0613@gmail.com>
2018-08-15 21:40:09 +0100

    add patient class skeleton

---
+++ patient.cpp
@@ -0,0 +1,21 @@
+#include patient.h
+
+namespace Medisys
+{
+    class Patient
+    {
+      private:
+
+
+
+      public:
+        Patient(std::string name, int age, Gender gender):
+            m_name{name}, m_age{age}, m_gender{gender}
+        {
+        }
+
+
+
+    }
+}
+
---
+++ patient.h
@@ -0,0 +1,22 @@
+#include <string>
+
+#ifndef PATIENT_H
+#define PATIENT_H
+
+namespace Medisys
+{
+    class Patient
+    {
+      private:
+        std::string m_name = "";
+        int         m_age;
+        Gender      m_gender;
+      public:
+        Patient();
+
+
+
+
+    }
+}
+#endif
